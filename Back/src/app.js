d11271371cc692136b2b03107b4600c45e646c8c
Galbfran <francogalbiati984@gmail.com>
2023-09-14 18:12:45 -0300

    inicializo back

---
+++ Back/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');// modulo principal express
+const cookieParser = require('cookie-parser');//middleware para analizar las cookies en las solicitudes entrantes.
+const bodyParser = require('body-parser');//middleware utilizado para analizar los cuerpos de las solicitudes HTTP entrantes.
+const morgan = require('morgan');//middleware de registro de solicitudes HTTP. Registra detalles sobre cada solicitud que llega al servidor.
+const routes = require('./routes/index.js');// import definicion de las rutas
+
+// importa conf y conexion bas de datos
+
+const app = express();// instancia express
+app.use(morgan("dev"));
+
+app.get("/" , (req , res) => {
+    res.status(200).send("ok")
+})
+
+
+module.exports = app;
