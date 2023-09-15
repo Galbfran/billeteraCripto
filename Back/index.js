5b9f587be977c9a7151f374b9b7702d877c60d4f
Galbfran <francogalbiati984@gmail.com>
2023-09-15 17:39:18 -0300

    conectando base  de datos

--- Back/index.js
+++ Back/index.js
@@ -1,8 +1,9 @@
 const app = require("./src/app");
-
+const {sequelize} = require("./src/db")
 const PORT = 3001;
 
 
 app.listen(PORT , () => {
+    sequelize.sync();
     console.log(`servidor en puerto ${PORT}`);
 })
--- Back/src/db.js
+++ Back/src/db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
-const { DB_NAME ,DB_PASSWORD , DB_USER} = process.env;
+const { DB_NAME ,DB_PASSWORD , DB_USER , DB_HOST} = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}/${DB_NAME}`)
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
+
+module.exports = {sequelize};
