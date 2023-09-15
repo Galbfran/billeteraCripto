4dd67ae90a08f6773e562100643b745d86f39532
Galbfran <francogalbiati984@gmail.com>
2023-09-15 16:56:55 -0300

    creando rutas users

--- Back/src/handlers/usersHandlers.js
+++ Back/src/handlers/usersHandlers.js
@@ -1,10 +1,33 @@
 
 
 
+
 const getUsersHandler = (req , res) =>{
     res.send("estoy en users")
 }
 
+const postUsersHandler = (req , res) =>{
+    res.send("ruta post users")
+}
+
+const getOneUserHandler = (req , res) =>{
+    res.send("ruta get users por id")
+}
+
+const updateOneUserHandler = (req , res) =>{
+    res.send("ruta update users por id")
+}
+
+const deleteUserHandler = (req , res) =>{
+    res.send("ruta delete users por id")
+}
+
+
+
 module.exports= {
-    getUsersHandler
+    getUsersHandler,
+    postUsersHandler,
+    getOneUserHandler,
+    updateOneUserHandler,
+    deleteUserHandler
 }
--- Back/src/routes/usersRoutes.js
+++ Back/src/routes/usersRoutes.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
-const {getUsersHandler} = require("./../handlers/usersHandlers")
+const {getUsersHandler , postUsersHandler , getOneUserHandler , updateOneUserHandler, deleteUserHandler} = require("./../handlers/usersHandlers")
 const usersRoutes = Router();
 
 
 
 
 usersRoutes.get("/" , getUsersHandler);
+
+usersRoutes.get("/:idUser" , getOneUserHandler);
+
+usersRoutes.post("/" , postUsersHandler);
+
+usersRoutes.put("/:idUser" , updateOneUserHandler);
+
+usersRoutes.delete("/:idUser", deleteUserHandler);
+
 
 module.exports = usersRoutes
