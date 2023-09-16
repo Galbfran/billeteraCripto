f1dd38fb49cefea55e962dec8d35b13bab23b053
Galbfran <francogalbiati984@gmail.com>
2023-09-16 15:34:40 -0300

    defino modelo user

--- Back/index.js
+++ Back/index.js
@@ -4,6 +4,6 @@ const PORT = 3001;
 
 
 app.listen(PORT , () => {
-    sequelize.sync();
+    sequelize.sync({force:true});
     console.log(`servidor en puerto ${PORT}`);
 })
--- Back/src/db.js
+++ Back/src/db.js
@@ -1,9 +1,33 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 require("dotenv").config();
 
 const { DB_NAME ,DB_PASSWORD , DB_USER , DB_HOST} = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+{logging:false});
+
+sequelize.define("User", {
+    idUser:{
+        type:DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    billetera:{
+        type: DataTypes.JSON
+    }
+})
 
 module.exports = {sequelize};
